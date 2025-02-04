0584d003e199c192c157d23a7dad2bad61dde36a
abed7helmi <abed.helmi@yahoo.fr>
2025-02-04 19:53:43 +0100

    les entités

---
+++ src/main/java/com/example/ebankingbackend/entities/Customer.java
@@ -0,0 +1,24 @@
+package com.example.ebankingbackend.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    @OneToMany(mappedBy = "customer")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private List<BankAccount> bankAccounts;
+}

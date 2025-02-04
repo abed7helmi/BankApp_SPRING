4e67ba55912c50a6727dfa0b59d3b12d100f434e
abed7helmi <abed.helmi@yahoo.fr>
2025-02-04 18:58:26 +0100

    init project

---
+++ src/main/java/com/example/ebankingbackend/EBankingBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebankingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBankingBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBankingBackEndApplication.class, args);
+    }
+
+}
